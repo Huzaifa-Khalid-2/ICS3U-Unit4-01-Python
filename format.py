1bf281409b890f99882b388774a4c68ec45210f1
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-04-08 17:11:53 -0400

    Update format.py

--- format.py
+++ format.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 
-# Created by: Mr. Coxall
-# Created on: Sep 2020
+# Created by: Huzaifa Khalid
+# Created on: April 2022
 # This program shows formatting output
 
 
 def main():
-    # this function shows formatting output
+    # this function uses a while loop
+    loop_counter = 0
+    total = 0
 
     # input
-    value = float(input("Enter a value ($): "))
-
-    # process
-    # nill
-
-    # output
+    user_number_as_string = int(input("Enter a positive integer: "))
     print("")
-    print("The cost is: ${:,.2f}".format(value))
-
+    # process & output
+    try:
+        user_number_as_integer = int(user_number_as_string)
+        while loop_counter < user_number_as_integer:
+            loop_counter = loop_counter + 1
+            total = total + loop_counter
+        print("The sum of is {0}".format(total))
+    except Exception:
+        print("¯\_(ツ)_/¯ sorry fam not an integer.")
     print("\nDone.")
 
 
